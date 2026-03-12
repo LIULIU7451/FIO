4d49f332db7fb08d45d5c161899f7887c9c0731a
LIULIU7451 <liuliu7450@gmail.com>
2026-03-12 11:13:26 +0800

    Update fio.sh

--- fio.sh
+++ fio.sh
@@ -1,7 +1,7 @@
-fio --name=seq_write_test --directory=/mnt/nfs --ioengine=libaio --direct=1 --rw=write --norandommap --randrepeat=0 --refill_buffers=1 --cpus_allowed=1 --bs=128k --iodepth=64 --numjobs=1 --size=10G --loops=1 --runtime=120 --overwrite=1 --time_based --group_reporting | tee -a SW.log
+fio --name=seq_write_test --directory=/mnt/nfs --ioengine=libaio --direct=1 --rw=write --norandommap --randrepeat=0 --refill_buffers=1 --cpus_allowed=1 --bs=128k --iodepth=64 --numjobs=1 --size=10G --loops=1 --runtime=120 --overwrite=1 --time_based --group_reporting | tee -a sw.log
 sleep 30
-fio --name=seq_read_test --directory=/mnt/nfs --ioengine=libaio --direct=1 --rw=read --bs=128k --iodepth=64 --numjobs=1  --refill_buffers=1 --cpus_allowed=1 --size=10G --loops=1 --runtime=120 --overwrite=1 --time_based --group_reporting | tee -a SR.log
+fio --name=seq_read_test --directory=/mnt/nfs --ioengine=libaio --direct=1 --rw=read --bs=128k --iodepth=64 --numjobs=1  --refill_buffers=1 --cpus_allowed=1 --size=10G --loops=1 --runtime=120 --overwrite=1 --time_based --group_reporting | tee -a sr.log
 sleep 30
-fio --name=rand_write_test --directory=/mnt/nfs --ioengine=libaio --direct=1 --rw=randwrite --norandommap --randrepeat=0  --refill_buffers=1 --cpus_allowed=1 --bs=4k --iodepth=64 --numjobs=16 --size=10G --loops=1 --runtime=120 --overwrite=1 --time_based --group_reporting | tee -a RW.log
+fio --name=rand_write_test --directory=/mnt/nfs --ioengine=libaio --direct=1 --rw=randwrite --norandommap --randrepeat=0  --refill_buffers=1 --cpus_allowed=1 --bs=4k --iodepth=64 --numjobs=16 --size=10G --loops=1 --runtime=120 --overwrite=1 --time_based --group_reporting | tee -a rw.log
 sleep 30
-fio --name=rand_read_test --directory=/mnt/nfs --ioengine=libaio --direct=1 --rw=randread --bs=4k --iodepth=64 --numjobs=16  --refill_buffers=1 --cpus_allowed=1  --size=10G --loops=1 --runtime=120 --overwrite=1 --time_based --group_reporting | tee -a RR.log
+fio --name=rand_read_test --directory=/mnt/nfs --ioengine=libaio --direct=1 --rw=randread --bs=4k --iodepth=64 --numjobs=16  --refill_buffers=1 --cpus_allowed=1  --size=10G --loops=1 --runtime=120 --overwrite=1 --time_based --group_reporting | tee -a rr.log
